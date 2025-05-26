64244c51aa94fd79cc3c5f04c55360278fca906b
ERATZCOBRA <154037761+ERATZCOBRA@users.noreply.github.com>
2025-05-26 23:36:52 +0530

    Update terminate.js

--- commands/terminate.js
+++ commands/terminate.js
@@ -74,7 +74,7 @@ module.exports = {
       return interaction.followUp({ content: '❌ Termination log channel not found.', ephemeral: true });
     }
 
-    const blueLine = '<:BlueLine:1294034867230736425>'.repeat(24);
+    const blueLine = '<:BlueLine:1372978644770750577>'.repeat(24);
 
     const embed = new EmbedBuilder()
       .setTitle('ㅤㅤㅤㅤㅤㅤ<:FBI_Badge:1192100309137375305>  FBI Termination Notice  <:FBI_Badge:1192100309137375305>ㅤㅤㅤㅤㅤㅤ')
